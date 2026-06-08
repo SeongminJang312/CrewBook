ca1a49a4b8e17738f4b8c02d1fe3614216053e5f
SeongminJang312 <jangseongmin0312@gmail.com>
2026-06-08 21:19:11 +0900

    Initial Commit

---
+++ CrewBook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CrewBook
+//
+//  Created by 장성민 on 6/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CrewBookTests/CrewBookTests.swift
@@ -0,0 +1,19 @@
+//
+//  CrewBookTests.swift
+//  CrewBookTests
+//
+//  Created by 장성민 on 6/8/26.
+//
+
+import Testing
+@testable import CrewBook
+
+struct CrewBookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
